12c498b3956e7629289bc06b86fbdd7beff952a2
Pavel Liavitski <pashalevy@gmail.com>
2024-05-15 09:32:45 +0200

    rotating nav my version added

---
+++ 03_rotating_navigation/my-version/script.js
@@ -0,0 +1,13 @@
+const openBtn = document.querySelector('#open');
+const closeBtn = document.querySelector('#close');
+const container = document.querySelector('.container');
+
+openBtn.addEventListener('click', open);
+
+function open() {
+  container.classList.add('show-nav');
+}
+
+closeBtn.addEventListener('click', () => {
+  container.classList.remove('show-nav');
+});
