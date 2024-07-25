424a209199b17abefea2a6bc1e943dcbae6842a8
Pavel Liavitski <pashalevy@gmail.com>
2024-07-25 12:35:23 +0200

    updated

---
+++ 32_good_cheap_fast_checkboxes/my-version/script.js
@@ -0,0 +1,24 @@
+const good = document.getElementById('good');
+const cheap = document.getElementById('cheap');
+const fast = document.getElementById('fast');
+const toggles = document.querySelectorAll('.toggle');
+
+toggles.forEach((toggle) =>
+  toggle.addEventListener('change', (e) => doTheTrick(e.target))
+);
+
+function doTheTrick(theClickedOne) {
+  if (good.checked && cheap.checked && fast.checked) {
+    if (good === theClickedOne) {
+      fast.checked = false;
+    }
+
+    if (cheap === theClickedOne) {
+      good.checked = false;
+    }
+
+    if (fast === theClickedOne) {
+      cheap.checked = false;
+    }
+  }
+}
